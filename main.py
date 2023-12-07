cc4aa0c29b41fa649ff3e1f97d9d5be1116971d5
rafi030615 <rafiandhika@student.telkomuniversity.ac.id>
2023-12-07 15:51:25 +0700

    update

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(
+        page_title = "RafiGPT",
+        page_icon="🤖",
+        layout="wide"
+    )
+    
+    st.header("RafiGPT")
+if __name__ == '__main__':
+    main()
