558b0b5f4d3b88de7fe4fce061833fab946909bf
rafi030615 <rafiandhika@student.telkomuniversity.ac.id>
2023-12-07 17:25:03 +0700

    update

--- main.py
+++ main.py
@@ -1,67 +1,26 @@
 import streamlit as st
-from streamlit_chat import message
-from dotenv import load_dotenv
-import os
-
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import (
-    SystemMessage,
-    HumanMessage,
-    AIMessage
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from decouple import config
+
+promt = PromptTemplate(
+    input_variables=["chat_history"]
 )
 
+st.set_page_config(
+    page_title="RafiGPT-1.0",
+    layout="wide"
+)
 
-def init():
-    # Load the OpenAI API key from the environment variable
-    load_dotenv()
-    
-    # test that the API key exists
-    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
-        print("OPENAI_API_KEY is not set")
-        exit(1)
-    else:
-        print("OPENAI_API_KEY is set")
+st.title("RafiGPT-1.0")
 
-    # setup streamlit page
-    st.set_page_config(
-        page_title="Your own ChatGPT",
-        page_icon="🤖"
-    )
-
-
-def main():
-    init()
-
-    chat = ChatOpenAI(temperature=0)
-
-    # initialize message history
-    if "messages" not in st.session_state:
-        st.session_state.messages = [
-            SystemMessage(content="You are a helpful assistant.")
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [
+        {"role" : "assistant", "content" : "Hello there!"}
         ]
-
-    st.header("Your own ChatGPT 🤖")
-
-    # sidebar with user input
-    with st.sidebar:
-        user_input = st.text_input("Your message: ", key="user_input")
-
-        # handle user input
-        if user_input:
-            st.session_state.messages.append(HumanMessage(content=user_input))
-            with st.spinner("Thinking..."):
-                response = chat(st.session_state.messages)
-            st.session_state.messages.append(
-                AIMessage(content=response.content))
-
-    # display message history
-    messages = st.session_state.get('messages', [])
-    for i, msg in enumerate(messages[1:]):
-        if i % 2 == 0:
-            message(msg.content, is_user=True, key=str(i) + '_user')
-        else:
-            message(msg.content, is_user=False, key=str(i) + '_ai')
-
-
-if __name__ == '__main__':
-    main()
+    
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
