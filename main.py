3eb2a0c496a64d9c14d8b89225d044fc30e5f889
rafi030615 <rafiandhika@student.telkomuniversity.ac.id>
2023-12-07 18:47:11 +0700

    update

--- main.py
+++ main.py
@@ -1,59 +1,37 @@
+from openai import OpenAI
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
-from decouple import config
-from langchain.memory import ConversationBufferWindowMemory
 
-prompt = PromptTemplate(
-    input_variables=["chat_history", "question"],
-    template="""You are a very kindl and friendly AI assistant. You are
-    currently having a conversation with a human. Answer the questions
-    in a kind and friendly tone with some sense of humor.
-    
-    chat_history: {chat_history},
-    Human: {question}
-    AI:"""
-)
+st.title("ChatGPT-like clone")
 
-llm = ChatOpenAI(openai_api_key=config("OPENAI_API_KEY"))
-memory = ConversationBufferWindowMemory(memory_key="chat_history", k=4)
-llm_chain = LLMChain(
-    llm=llm,
-    memory=memory,
-    prompt=prompt
-)
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
-st.set_page_config(
-    page_title="ChatGPT Clone",
-    page_icon="🤖",
-    layout="wide"
-)
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-st.title("ChatGPT Clone")
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-# check for messages in session and create if not exists
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [
-        {"role": "assistant", "content": "Hello there, am ChatGPT clone"}
-    ]
-
-# Display all messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
-        st.write(message["content"])
+        st.markdown(message["content"])
 
-user_prompt = st.chat_input()
-
-if user_prompt is not None:
-    st.session_state.messages.append({"role": "user", "content": user_prompt})
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
-        st.write(user_prompt)
-        
-if st.session_state.messages[-1]["role"] != "assistant":
+        st.markdown(prompt)
+
     with st.chat_message("assistant"):
-        with st.spinner("Loading..."):
-            ai_response = llm_chain.predict(question=user_prompt)
-            st.write(ai_response)
-    new_ai_message = {"role": "assistant", "content": ai_response}
-    st.session_state.messages.append(new_ai_message)
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += (response.choices[0].delta.content or "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
