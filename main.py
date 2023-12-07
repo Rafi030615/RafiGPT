f32fbe74631e9b0b3dc661f7ac5cb625b3dd5cf4
rafi030615 <rafiandhika@student.telkomuniversity.ac.id>
2023-12-07 17:53:58 +0700

    update

--- main.py
+++ main.py
@@ -5,8 +5,17 @@ from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from decouple import config
 
-promt = PromptTemplate(
-    input_variables=["chat_history"]
+prompt = PromptTemplate(
+    input_variables=["chat_history", "question"],
+    template="""test"""
+)
+
+llm = ChatOpenAI(openai_api_key=config("OPENAI_API_KEY"))
+memory = ConversationBufferWindowMemory(memory_key="chat_history", k=5)
+llm_chain = LLMChain(
+    llm=llm,
+    memory=memory,
+    prompt=prompt
 )
 
 st.set_page_config(
@@ -24,3 +33,18 @@ if "messages" not in st.session_state.keys():
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
+        
+user_prompt = st.chat_input()
+
+if user_prompt is not None:
+    st.session_state.messages.append({"role" : "assistant", "content" : "Hello there!"})
+    with st.chat_message("user"):
+        st.write(user_prompt)
+
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Loading...."):
+            ai_response = llm_chain.predict(question=user_prompt)
+            st.write(ai_response)
+    new_ai_message = {"role" : "assistant", "content" : ai_response}
+    st.session_state.messages.append(new_ai_message)
